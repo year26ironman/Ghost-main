95af8811a6e00f1cf6214ece5c35475f91813a3e
AD-007 <gyanprakasht98726@gmail.com>
2026-09-03 19:33:58 +0530

    Update vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     include: ['tests/**/*.test.ts'],
-    exclude: ['bboard/**', 'node_modules/**'],
+    exclude: ['bboard/**', 'node_modules/**'], 
   },
 });
