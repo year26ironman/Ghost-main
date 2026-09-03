56ce28fc3095cb54c000f926c615d3be2f9a743c
AD-007 <gyanprakasht98726@gmail.com>
2026-09-03 19:41:35 +0530

    Update index.d.ts

--- managed/contract/index.d.ts
+++ managed/contract/index.d.ts
@@ -1,7 +1,7 @@
 import type * as __compactRuntime from '@midnight-ntwrk/compact-runtime';
 
 export type Witnesses<PS> = {
-}
+} 
 
 export type ImpureCircuits<PS> = {
   spend(context: __compactRuntime.CircuitContext<PS>, amount_0: bigint): __compactRuntime.CircuitResults<PS, []>;
