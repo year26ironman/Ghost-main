4df85a22df33cf8bd872f50eb2426b95f8f72023
AD-007 <gyanprakasht98726@gmail.com>
2026-09-03 19:35:51 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
+ 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
