a4bf786a2b75ba2e2fee850cf978d5f7170d4b3e
AD-007 <gyanprakasht98726@gmail.com>
2026-09-03 19:42:57 +0530

    Update GlassCard.tsx

--- components/ui/GlassCard.tsx
+++ components/ui/GlassCard.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { cn } from '@/lib/utils';
 import { motion, HTMLMotionProps } from 'framer-motion';
-
+ 
 interface GlassCardProps extends HTMLMotionProps<"div"> {
   children: React.ReactNode;
   className?: string;
